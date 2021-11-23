2791a5df76a6d66e7af9ac395e8ae8e277f29699
Nikola Đuza <nikolaseap@gmail.com>
2021-11-23 13:25:27 +0100

    Finish Exercise 2 subtask: Story borders using grid

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import {
-  MAIN_STORY,
-  OPINION_STORIES,
-  SECONDARY_STORIES,
-} from '../../data';
+import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from "../../data";
 
-import SectionTitle from '../SectionTitle';
-import MainStory from '../MainStory';
-import SecondaryStory from '../SecondaryStory';
-import OpinionStory from '../OpinionStory';
-import Advertisement from '../Advertisement';
+import SectionTitle from "../SectionTitle";
+import MainStory from "../MainStory";
+import SecondaryStory from "../SecondaryStory";
+import OpinionStory from "../OpinionStory";
+import Advertisement from "../Advertisement";
 
 const MainStoryGrid = () => {
   return (
@@ -23,7 +19,11 @@ const MainStoryGrid = () => {
       <SecondaryStorySection>
         <StoryList>
           {SECONDARY_STORIES.map((story, index) => (
-            <SecondaryStory key={story.id} {...story} />
+            <>
+              <SecondaryStory key={story.id} {...story} />
+
+              {index !== SECONDARY_STORIES.length - 1 && <Spacer />}
+            </>
           ))}
         </StoryList>
       </SecondaryStorySection>
@@ -32,7 +32,10 @@ const MainStoryGrid = () => {
         <SectionTitle>Opinion</SectionTitle>
         <StoryList>
           {OPINION_STORIES.map((story, index) => (
-            <OpinionStory key={story.id} {...story} />
+            <>
+              <OpinionStory key={story.id} {...story} />
+              {index !== OPINION_STORIES.length - 1 && <Spacer />}
+            </>
           ))}
         </StoryList>
       </OpinionSection>
@@ -47,10 +50,10 @@ const MainStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   grid-template-areas:
-    'main-story'
-    'secondary-stories'
-    'opinion-stories'
-    'advertisement';
+    "main-story"
+    "secondary-stories"
+    "opinion-stories"
+    "advertisement";
   gap: 48px;
   margin-bottom: 48px;
 `;
@@ -63,9 +66,15 @@ const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
 `;
 
+const Spacer = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: var(--color-gray-300);
+`;
+
 const StoryList = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  gap: 16px;
 `;
 
 const OpinionSection = styled.section`
